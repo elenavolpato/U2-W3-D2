8f9755a6e32097dc11fb92d1a927939cbc06a41e
raposinha <elena@raposinha.dev>
2026-01-20 17:02:15 +0100

    finishes the timer with minutes and seconds

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const removeName = () => {
 };
 
 const timerEl = document.getElementById("timer");
-
 let elapsedTime = sessionStorage.getItem("timeCounter");
 
 if (elapsedTime === null) {
@@ -33,10 +32,24 @@ if (elapsedTime === null) {
   elapsedTime = Number(elapsedTime);
 }
 
-timerEl.textContent = elapsedTime;
+const formatTime = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+
+  //makes the number always have 2 characters .padStart()
+  const mm = minutes.toString().padStart(2, "0");
+  const ss = secs.toString().padStart(2, "0");
+
+  return mm + ":" + ss;
+};
+
+timerEl.textContent = formatTime(elapsedTime);
 
 setInterval(() => {
+  const date = new Date();
+  const sec = date.getSeconds();
+  const min = date.getMinutes();
   elapsedTime++;
   sessionStorage.setItem("timeCounter", elapsedTime);
-  timerEl.textContent = elapsedTime;
+  timerEl.textContent = formatTime(elapsedTime);
 }, 1000);
